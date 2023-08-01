ea9843c4f3f2ad26dc5752bb86f9b3861d0ad360
alexbotha <alexbotha@hotmail.co.uk>
2023-08-01 17:27:52 +0100

    commit tree a6ec9ff45a8db45be26a4eea7c2720043eb6937d

--- lib/first_spicy_batch.rb
+++ lib/first_spicy_batch.rb
@@ -1,8 +1,29 @@
 # The is_spicy_batch method is already defined for you and will be called when running tests.
 # @param {Integer} batch
 # @return {boolean} whether the batch is spicy
-# def is_spicy_batch(batch):
+ 
 
 def first_spicy_batch(n)
-  # your code here
+
+  left = 0 
+  right = n 
+
+  while left < right 
+
+    mid = left + (right - left) / 2
+
+    if is_spicy_batch(mid)
+      right = mid 
+    else 
+      left = mid + 1
+    end 
+  end 
+ left 
 end
+
+
+# we have an array of batches, they are sorted in order. So at some point in the array we will hit a batch that will be spicey and the batches thereafter will also be spicy. 
+
+# We can set left to 0, and then set right to n which is the nth number in the batches array.
+
+# We can then say if left is less than the nth number, then we want to halve the array. We can continue to halve the array until we are able to narrow down to the first batch
